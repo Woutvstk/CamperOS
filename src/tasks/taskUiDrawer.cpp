d949b93ca7d80399049e3ca73edfd0a8a186b323
Woutvstk <119763111+Woutvstk@users.noreply.github.com>
2025-09-15 17:36:35 +0200

    clean up null pointer checks in taskUiDrawer

--- src/tasks/taskUiDrawer.cpp
+++ src/tasks/taskUiDrawer.cpp
@@ -3,37 +3,43 @@
 void taskUiDrawer(void *parameter)
 {
 
+    bool fail = false;
+
     while (true)
     {
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // wait for notification from taskUiController to start drawing
 
         SdrawerInstruction currentInstruction;
 
-        if (xQueueReceive(QtaskUIController2taskDrawer, &currentInstruction, 0)) // check if something was received from queue
+        if (!xQueueReceive(QtaskUIController2taskDrawer, &currentInstruction, 0)) // check if something was received from queue
         {
-            if (&currentInstruction == nullptr)
-            {
-                Serial.println("currentInstruction is a NULL pointer, not drawing");
-            }
-            else
-            {
-                if ((currentInstruction.page) == nullptr)
-                {
-                    Serial.println("currentInstruction.page is a null pointer, not draWing");
-                }
-                else
-                {
-                    if (currentInstruction.screen == nullptr)
-                    {
-                        Serial.println(" ####### currentInstruction.screen is a nullptr");
-                    }
-                    else
-                    {
-
-                        (currentInstruction.page)->draw(currentInstruction.screen);
-                    }
-                }
-            }
+            Serial.println(" [taskUiDrawer] - received notification but no item in queue, not drawing");
+            fail = true;
         }
+
+        if (&currentInstruction == nullptr)
+        {
+            Serial.println(" [taskUiDrawer] - currentInstruction is a NULL pointer, not drawing");
+            fail = true;
+        }
+
+        if ((currentInstruction.page) == nullptr)
+        {
+            Serial.println(" [taskUiDrawer] - currentInstruction.page is a null pointer, not draWing");
+            fail = true;
+        }
+
+        if (currentInstruction.screen == nullptr)
+        {
+            Serial.println(" [taskUiDrawer] - currentInstruction.screen is a nullptr, not drawing");
+            fail = true;
+        }
+
+        if (!fail)
+        {
+            (currentInstruction.page)->draw(currentInstruction.screen);
+        }
+
+        fail = false;
     }
 }
