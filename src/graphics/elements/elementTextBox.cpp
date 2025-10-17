acbcfb82457ef54051bdf19371907853914a3b5c
Woutvstk <119763111+Woutvstk@users.noreply.github.com>
2025-10-17 13:26:23 +0200

    Add padding to textbox

--- src/graphics/elements/elementTextBox.cpp
+++ src/graphics/elements/elementTextBox.cpp
@@ -6,6 +6,7 @@ namespace graphics
     bool elementTextBox::draw(GFXcanvas16 *screen)
     {
         // use temporary canvas so that text wrapping can do its thing
+
         GFXcanvas16 textBox = GFXcanvas16(size_x_px, size_y_px);
         if (enableFill)
         {
@@ -21,13 +22,38 @@ namespace graphics
             textBox.drawRect(i, i, textBox.width() - 2 * i, textBox.height() - 2 * i, borderColor);
         }
 
-        textBox.setCursor(0, 0);
-        textBox.setTextSize(textSize);
-        textBox.setTextColor(textColor);
-        textBox.print(text);
+        printString(&textBox, text.c_str(), text.length());
 
         screen->drawRGBBitmap(pos_x_px, pos_y_px, textBox.getBuffer(), size_x_px, size_y_px);
         return false;
     };
+
+    void elementTextBox::printString(GFXcanvas16 *textBox, const char *buffer, uint16_t size)
+    {
+        uint16_t cursor_x = padding + borderWidth;
+        uint16_t cursor_y = padding + borderWidth;
+
+        while (size--)
+        {
+            uint8_t c = *buffer;
+            if (c == '\n')
+            {                                     // Newline?
+                cursor_x = padding + borderWidth; // Reset x to zero,
+                cursor_y += textSize * 8;         // advance y one line
+            }
+            else if (c != '\r')
+            { // Ignore carriage returns
+                if ((cursor_x + textSize * 6 + padding + borderWidth) > size_x_px)
+                {                                     // Off right?
+                    cursor_x = padding + borderWidth; // Reset x to zero,
+                    cursor_y += textSize * 8;         // advance y one line
+                }
+                textBox->drawChar(cursor_x, cursor_y, c, textColor, textColor, textSize,
+                                  textSize);
+                cursor_x += textSize * 6; // Advance x one char
+            }
+            buffer++;
+        }
+    }
 
 }
--- src/graphics/elements/elementTextBox.h
+++ src/graphics/elements/elementTextBox.h
@@ -14,9 +14,13 @@ namespace graphics
         uint16_t fillColor = 0x0000;
         uint8_t borderWidth = 0;
         uint16_t borderColor = 0xFFFF;
+        uint16_t padding = 3;
 
         elementTextBox(uint16_t pos_x_px, uint16_t pos_y_px, uint16_t size_x_px, uint16_t size_y_px, uint16_t color)
             : pageElement(pos_x_px, pos_y_px, size_x_px, size_y_px), textColor(color) {};
         bool draw(GFXcanvas16 *screen) override;
+
+    private:
+        void printString(GFXcanvas16 *textBox, const char *buffer, uint16_t size);
     };
 }
